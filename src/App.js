2ca7d431e38ddd7c2c5c1d3f07b7bcb3e36b9c40
Kavin abregana <abreganakavin@gmail.com>
2025-02-28 20:16:19 +0800

    adding seo feature

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   //Instance ID
   let InstanceId_Widget = "505703";
   let InstanceId_QA = "";
-  let InstanceId_SEO = "";
+  let InstanceId_SEO = "1048446";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "529946";
   let InstanceId_PromotedProd = "";
